e7760a63236152f200acefdbc3f2d6d019b10cba
lvvvmd <22552826@qq.com>
2018-04-13 01:17:11 +0800

    Create Solution.cpp

---
+++ Ransom Note/Solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int a[26];
+        for(int i = 0;i<26;i++)
+            a[i] = 0;
+        
+        for(int i = 0;i<magazine.length();i++){
+            a[magazine[i] - 'a']++;
+        }
+         for(int i = 0;i<ransomNote.length();i++){
+            a[ransomNote[i] - 'a']--;
+            if(a[ransomNote[i] - 'a'] < 0)
+                return 0;
+        }
+        return 1;
+    }
+};
