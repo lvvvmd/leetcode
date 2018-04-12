88249e218195837905a30867a6d5a4b2dd082a85
lvvvmd <22552826@qq.com>
2018-04-12 20:10:44 +0800

    Create Solution.cpp

---
+++ Lexicographical Numbers/Solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+		vector<int> rst;
+		rst.resize(n);
+		int cur = 1;
+		for (int i = 0; i < n; i++){
+			//cout << cur << " ";
+			rst[i] = cur;
+			if (cur * 10 <= n)
+				cur *= 10;
+			else{
+				if (cur == n)
+				{
+					cur /= 10;
+				}
+                cur++;
+				while (cur % 10 == 0)
+					cur /= 10;
+			}
+		}
+		return rst;
+}
+};
